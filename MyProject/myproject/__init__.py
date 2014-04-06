4a8828a6dbc45de0d39c1106b5fb87832b5a5ac7
Mirann <annaibakaeva@google.com>
2014-04-06 10:26:23 +0600

    + search

--- MyProject/myproject/__init__.py
+++ MyProject/myproject/__init__.py
@@ -9,5 +9,6 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('registration', '/registration')
+    config.add_route('search', '/search')
     config.scan()
     return config.make_wsgi_app()
--- MyProject/myproject/views.py
+++ MyProject/myproject/views.py
@@ -1,11 +1,15 @@
 from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/home.jinja2')
-def my_view2(request):
+def my_view_home(request):
     return {'project': 'MyProject'}
 
 @view_config(route_name='registration', renderer='templates/regist.jinja2')
 def my_view(request):
+    return {'project': 'MyProject'}
+
+@view_config(route_name='search', renderer='templates/search.jinja2')
+def my_view_search(request):
     return {'project': 'MyProject'}
 
 
