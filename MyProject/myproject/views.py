443b30fb3e0db74fad0f64ca0d3e75fe60ee589e
Mirann <annaibakaeva@google.com>
2014-04-18 18:30:01 +0600

    + design my_trees

--- MyProject/myproject/views.py
+++ MyProject/myproject/views.py
@@ -12,4 +12,14 @@ def my_view(request):
 def my_view_search(request):
     return {'project': 'MyProject'}
 
+@view_config(route_name='profile', renderer='templates/profile.jinja2')
+def my_view_profile(request):
+    return {'project': 'MyProject'}
 
+@view_config(route_name='about', renderer='templates/about.jinja2')
+def my_view_about(request):
+    return {'project': 'MyProject'}
+
+@view_config(route_name='trees', renderer='templates/my_trees.jinja2')
+def my_view_trees(request):
+    return {'project': 'MyProject'}
